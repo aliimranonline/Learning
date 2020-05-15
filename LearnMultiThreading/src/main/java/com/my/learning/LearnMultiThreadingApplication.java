c5f8865e6d79f7036eda5612b84dbd13606f0bfc
aliimranonline <55789720+aliimranonline@users.noreply.github.com>
2020-05-15 15:29:03 +0500

    Initial Commit
    For Multithreading

---
+++ LearnMultiThreading/src/main/java/com/my/learning/LearnMultiThreadingApplication.java
@@ -0,0 +1,13 @@
+package com.my.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMultiThreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMultiThreadingApplication.class, args);
+	}
+
+}
